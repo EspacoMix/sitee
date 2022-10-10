3af861448a55138085cf67d04c2dd9c66500b354
TodosJuntosJAA <105557405+TodosJuntosJAA@users.noreply.github.com>
2022-10-09 23:11:29 -0300

    Add files via upload

---
+++ js/mask.js
@@ -0,0 +1,26 @@
+function telefone_mask() {
+    var tamanho = document.getElementById('telefone').value.length;
+    var telefone = document.getElementById('telefone').value;
+    if (tamanho == 2) {
+        telefone = '(' + telefone + ')';
+    }
+    else if (tamanho == 9) {
+        telefone += '-';
+    }
+    document.getElementById('telefone').value = telefone;
+}
+
+function cpf_maskara() {
+    var tamanho = document.getElementById('cpf').value.length;
+    var cpf = document.getElementById('cpf').value;
+    if (tamanho == 3) {
+        cpf += ".";
+    }
+    else if (tamanho == 7) {
+        cpf += '.';
+    }
+    else if (tamanho == 11) {
+        cpf += '-';
+    }
+    document.getElementById('cpf').value = cpf;
+}
